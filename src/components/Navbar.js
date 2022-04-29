2dd606da1d9fe81dec2db7f45460d701639db0fb
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-04-29 21:24:07 +0700

    adding the Sidebar component to the Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,59 @@
 import { useState } from "react";
+import Sidebar from "./Sidebar";
+
+import { faHome, faList, faCog } from "@fortawesome/free-solid-svg-icons";
 
 export default function Navbar() {
 	const [showSidebar, setShowSidebar] = useState(false);
 
+	function closeSidebar() {
+		setShowSidebar(false);
+	}
+
+	const links = [
+		{
+			name: "home",
+			path: "/",
+			icon: faHome,
+		},
+		{
+			name: "recipes",
+			path: "/recipes",
+			icon: faList,
+		},
+		{
+			name: "settings",
+			path: "/settings",
+			icon: faCog,
+		},
+	];
+
 	return (
-		<div className="navbar container">
-			<a href="#!" className="logo">
-				F<span>oo</span>d Hub
-			</a>
-			<div className="nav-links">
-				<a href="#!">Home</a>
-				<a href="#!">Recipes</a>
-				<a href="#!">Settings</a>
+		<>
+			<div className="navbar container">
+				<a href="#!" className="logo">
+					F<span>oo</span>d Hub
+				</a>
+				<div className="nav-links">
+					{/* <a href="#!">Home</a>
+					<a href="#!">Recipes</a>
+					<a href="#!">Settings</a> */}
+					{links.map((link) => (
+						<a href="#!" key={link.name}>
+							{link.name}
+						</a>
+					))}
+				</div>
+				<div
+					onClick={() => setShowSidebar(!showSidebar)}
+					className={showSidebar ? "sidebar-btn active" : "sidebar-btn"}
+				>
+					<div className="bar"></div>
+					<div className="bar"></div>
+					<div className="bar"></div>
+				</div>
 			</div>
-			<div
-				onClick={() => setShowSidebar(!showSidebar)}
-				className={showSidebar ? "sidebar-btn active" : "sidebar-btn"}
-			>
-				<div className="bar"></div>
-				<div className="bar"></div>
-				<div className="bar"></div>
-			</div>
-		</div>
+			{showSidebar && <Sidebar close={closeSidebar} links={links} />}
+		</>
 	);
 }
