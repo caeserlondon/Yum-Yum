26c9941cabfc548908a10cdc449e07864782e45b
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-04-29 19:31:59 +0700

    adding the Navbar component to the source

---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+export default function Navbar() {
+	const [showSidebar, setShowSidebar] = useState(false);
+
+	return (
+		<div className="navbar container">
+			<a href="#!" className="logo">
+				F<span>oo</span>d Hub
+			</a>
+			<div className="nav-links">
+				<a href="#!">Home</a>
+				<a href="#!">Recipes</a>
+				<a href="#!">Settings</a>
+			</div>
+			<div
+				onClick={() => setShowSidebar(!showSidebar)}
+				className={showSidebar ? "sidebar-btn active" : "sidebar-btn"}
+			>
+				<div className="bar"></div>
+				<div className="bar"></div>
+				<div className="bar"></div>
+			</div>
+		</div>
+	);
+}
