96ee62581fb0ef2b92f752e807e34005b82345f4
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-04-30 13:05:38 +0700

    creating the ImproveSkills section component

---
+++ src/components/ImproveSkills.js
@@ -0,0 +1,27 @@
+export default function ImproveSkills() {
+	const list = [
+		"discover new ingredient",
+		"Experiment with food",
+		"Know nutrition facts",
+		"Learn new recepies",
+		"Get cooking tips",
+		"Eat healthy",
+	];
+
+	return (
+		<div className="section improve-skills">
+			<div className="col img">
+				<img src="/img/gallery/img_10.jpeg" alt="sushi" />
+			</div>
+			<div className="col typography">
+				<h1 className="title">Improve Your Culinary Skills</h1>
+				{list.map((item, index) => (
+					<p className="skill-item" key={index}>
+						{item}
+					</p>
+				))}
+				<button className="btn">Let's Cook</button>
+			</div>
+		</div>
+	);
+}
