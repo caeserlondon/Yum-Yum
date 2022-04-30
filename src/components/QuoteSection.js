814530ca29aa50950efbc1b8fb141ef5d2f809ed
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-04-30 13:16:18 +0700

    creating the QueteSection component

---
+++ src/components/QuoteSection.js
@@ -0,0 +1,12 @@
+export default function QuoteSection() {
+	return (
+		<div className="section quote">
+			<p className="quote-text">
+				Cooking is like painting or writing a song. Just as there are only so
+				many notes or colors, there are only so many flavors - it's how you
+				combine them that sets you apart.
+			</p>
+			<p className="quote-auther">- Wolfgang Puck</p>
+		</div>
+	);
+}
