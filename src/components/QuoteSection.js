1600abb4b7f05e976388ed776ab74addaa458efd
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-04-30 13:39:30 +0700

    updating the QuoteSection component

--- src/components/QuoteSection.js
+++ src/components/QuoteSection.js
@@ -1,7 +1,11 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faQuoteLeft } from "@fortawesome/free-solid-svg-icons";
+
 export default function QuoteSection() {
 	return (
 		<div className="section quote">
 			<p className="quote-text">
+				<FontAwesomeIcon icon={faQuoteLeft} />
 				Cooking is like painting or writing a song. Just as there are only so
 				many notes or colors, there are only so many flavors - it's how you
 				combine them that sets you apart.
