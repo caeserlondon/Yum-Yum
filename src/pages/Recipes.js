f2922706269a8d501a04faafaa284ae0d500ab7d
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-01 17:51:55 +0700

    adding the previous searches section to the Recipes page

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,3 +1,42 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
 export default function Recipes() {
-	return <div>Recipes</div>;
+	const searches = [
+		"pizza",
+		"burger",
+		"cookies",
+		"juice",
+		"biriyani",
+		"salad",
+		"ice cream",
+		"lasagna",
+		"pudding",
+		"soup",
+	];
+
+	return (
+		<div>
+			<div className="previous-searches section">
+				<h2>Previous Searches</h2>
+				<div className="previous-searches-container">
+					{searches.map((search, index) => (
+						<div
+							className="search-item"
+							key={index}
+							style={{ animationDelay: index * 0.15 + "s" }}
+						>
+							{search}
+						</div>
+					))}
+				</div>
+				<div className="search-box">
+					<input type="text" placeholder="Search" />
+					<button className="btn">
+						<FontAwesomeIcon icon={faSearch} />
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 }
