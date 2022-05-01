3371addc4750384926b0804fad88eba08e38cef3
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-01 22:27:42 +0700

    Updating and Refracturing the Recipes page and adding the recipes

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,5 +1,79 @@
 import PreviousSearches from "../components/PreviousSearches";
+import RecipeCard from "../components/RecipeCard";
 
 export default function Recipes() {
-	return <PreviousSearches />;
+	const recipes = [
+		{
+			title: "Chicken Pan Pizza",
+			image: "/img/gallery/img_1.jpeg",
+			authorImg: "/img/top-chiefs/img_1.jpeg",
+		},
+		{
+			title: "Spaghetti and Meatballs",
+			image: "/img/gallery/img_4.jpeg",
+			authorImg: "/img/top-chiefs/img_2.jpeg",
+		},
+		{
+			title: "American Cheese Burger",
+			image: "/img/gallery/img_5.jpeg",
+			authorImg: "/img/top-chiefs/img_3.jpeg",
+		},
+		{
+			title: "Mutton Biriyani",
+			image: "/img/gallery/img_6.jpeg",
+			authorImg: "/img/top-chiefs/img_5.jpeg",
+		},
+		{
+			title: "Japanese Sushi",
+			image: "/img/gallery/img_10.jpeg",
+			authorImg: "/img/top-chiefs/img_6.jpeg",
+		},
+		{
+			title: "Chicken Pan Pizza",
+			image: "/img/gallery/img_1.jpeg",
+			authorImg: "/img/top-chiefs/img_1.jpeg",
+		},
+		{
+			title: "Spaghetti and Meatballs",
+			image: "/img/gallery/img_4.jpeg",
+			authorImg: "/img/top-chiefs/img_2.jpeg",
+		},
+		{
+			title: "American Cheese Burger",
+			image: "/img/gallery/img_5.jpeg",
+			authorImg: "/img/top-chiefs/img_3.jpeg",
+		},
+		{
+			title: "Mutton Biriyani",
+			image: "/img/gallery/img_6.jpeg",
+			authorImg: "/img/top-chiefs/img_5.jpeg",
+		},
+		{
+			title: "Japanese Sushi",
+			image: "/img/gallery/img_10.jpeg",
+			authorImg: "/img/top-chiefs/img_6.jpeg",
+		},
+		{
+			title: "American Cheese Burger",
+			image: "/img/gallery/img_5.jpeg",
+			authorImg: "/img/top-chiefs/img_3.jpeg",
+		},
+		{
+			title: "Mutton Biriyani",
+			image: "/img/gallery/img_6.jpeg",
+			authorImg: "/img/top-chiefs/img_5.jpeg",
+		},
+	].sort(() => Math.random() - 0.5);
+
+	return (
+		<div>
+			<PreviousSearches />
+			<div className="recipes-container">
+				{/* <RecipeCard /> */}
+				{recipes.map((recipe, index) => (
+					<RecipeCard key={index} recipe={recipe} />
+				))}
+			</div>
+		</div>
+	);
 }
