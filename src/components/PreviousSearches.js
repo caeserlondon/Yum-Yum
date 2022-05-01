71ad6bad65729e1be830dba11a2311b7dbcfb7a6
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-01 18:32:39 +0700

    creating the PreviousSearches component

---
+++ src/components/PreviousSearches.js
@@ -0,0 +1,40 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
+const searches = [
+	"pizza",
+	"burger",
+	"cookies",
+	"juice",
+	"biriyani",
+	"salad",
+	"ice cream",
+	"lasagna",
+	"pudding",
+	"soup",
+];
+
+export default function PreviousSearches() {
+	return (
+		<div className="previous-searches section">
+			<h2>Previous Searches</h2>
+			<div className="previous-searches-container">
+				{searches.map((search, index) => (
+					<div
+						className="search-item"
+						key={index}
+						style={{ animationDelay: index * 0.15 + "s" }}
+					>
+						{search}
+					</div>
+				))}
+			</div>
+			<div className="search-box">
+				<input type="text" placeholder="Search" />
+				<button className="btn">
+					<FontAwesomeIcon icon={faSearch} />
+				</button>
+			</div>
+		</div>
+	);
+}
