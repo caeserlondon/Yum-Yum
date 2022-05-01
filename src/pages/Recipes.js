9b1bd2deee200a748db0e568c31d9e6c8ccd5f7f
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-01 23:58:47 +0700

    Updating  the recipes in the Recipes page

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -4,52 +4,57 @@ import RecipeCard from "../components/RecipeCard";
 export default function Recipes() {
 	const recipes = [
 		{
-			title: "Chicken Pan Pizza",
+			title: "chicken tandoori",
 			image: "/img/gallery/img_1.jpeg",
 			authorImg: "/img/top-chiefs/img_1.jpeg",
 		},
 		{
+			title: "talian Meatballs",
+			image: "/img/gallery/img_20.jpeg",
+			authorImg: "/img/top-chiefs/img_27.jpeg",
+		},
+		{
 			title: "Spaghetti and Meatballs",
-			image: "/img/gallery/img_4.jpeg",
-			authorImg: "/img/top-chiefs/img_2.jpeg",
+			image: "/img/gallery/img_20.jpeg",
+			authorImg: "/img/top-chiefs/img_20.jpeg",
 		},
 		{
 			title: "American Cheese Burger",
 			image: "/img/gallery/img_5.jpeg",
-			authorImg: "/img/top-chiefs/img_3.jpeg",
+			authorImg: "/img/top-chiefs/img_23.jpeg",
 		},
 		{
 			title: "Mutton Biriyani",
 			image: "/img/gallery/img_6.jpeg",
-			authorImg: "/img/top-chiefs/img_5.jpeg",
+			authorImg: "/img/top-chiefs/img_13.jpeg",
 		},
 		{
 			title: "Japanese Sushi",
 			image: "/img/gallery/img_10.jpeg",
-			authorImg: "/img/top-chiefs/img_6.jpeg",
-		},
-		{
-			title: "Chicken Pan Pizza",
-			image: "/img/gallery/img_1.jpeg",
-			authorImg: "/img/top-chiefs/img_1.jpeg",
-		},
-		{
-			title: "Spaghetti and Meatballs",
-			image: "/img/gallery/img_4.jpeg",
-			authorImg: "/img/top-chiefs/img_2.jpeg",
-		},
-		{
-			title: "American Cheese Burger",
-			image: "/img/gallery/img_5.jpeg",
-			authorImg: "/img/top-chiefs/img_3.jpeg",
-		},
-		{
-			title: "Mutton Biriyani",
-			image: "/img/gallery/img_6.jpeg",
 			authorImg: "/img/top-chiefs/img_5.jpeg",
 		},
 		{
-			title: "Japanese Sushi",
+			title: "Pizza margherita",
+			image: "/img/gallery/img_1.jpeg",
+			authorImg: "/img/top-chiefs/img_2.jpeg",
+		},
+		{
+			title: "omurice vegetables",
+			image: "/img/gallery/img_4.jpeg",
+			authorImg: "/img/top-chiefs/img_3.jpeg",
+		},
+		{
+			title: "American Burger",
+			image: "/img/gallery/img_5.jpeg",
+			authorImg: "/img/top-chiefs/img_12.jpeg",
+		},
+		{
+			title: "greek salad",
+			image: "/img/gallery/img_6.jpeg",
+			authorImg: "/img/top-chiefs/img_4.jpeg",
+		},
+		{
+			title: "Ramen",
 			image: "/img/gallery/img_10.jpeg",
 			authorImg: "/img/top-chiefs/img_6.jpeg",
 		},
