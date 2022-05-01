aacd4beefb55c8e82fe6256d95d8ed9092142120
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-01 18:33:47 +0700

    Refracturing the Recipes page

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,42 +1,5 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import PreviousSearches from "../components/PreviousSearches";
 
 export default function Recipes() {
-	const searches = [
-		"pizza",
-		"burger",
-		"cookies",
-		"juice",
-		"biriyani",
-		"salad",
-		"ice cream",
-		"lasagna",
-		"pudding",
-		"soup",
-	];
-
-	return (
-		<div>
-			<div className="previous-searches section">
-				<h2>Previous Searches</h2>
-				<div className="previous-searches-container">
-					{searches.map((search, index) => (
-						<div
-							className="search-item"
-							key={index}
-							style={{ animationDelay: index * 0.15 + "s" }}
-						>
-							{search}
-						</div>
-					))}
-				</div>
-				<div className="search-box">
-					<input type="text" placeholder="Search" />
-					<button className="btn">
-						<FontAwesomeIcon icon={faSearch} />
-					</button>
-				</div>
-			</div>
-		</div>
-	);
+	return <PreviousSearches />;
 }
