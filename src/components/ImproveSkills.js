59810bb2bd7bbc3155c1914215318182101d137d
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-31 22:41:47 +0100

    Updating the layout

--- src/components/ImproveSkills.js
+++ src/components/ImproveSkills.js
@@ -1,17 +1,35 @@
+import CustomImage from './CustomImage';
+
 export default function ImproveSkills() {
 	const list = [
-		"discover new ingredient",
-		"Experiment with food",
-		"Know nutrition facts",
-		"Learn new recepies",
-		"Get cooking tips",
-		"Eat healthy",
+		'discover new ingredient',
+		'Experiment with food',
+		'Know nutrition facts',
+		'Learn new recepies',
+		'Get cooking tips',
+		'Eat healthy',
+	];
+	const images = [
+		'img/gallery/img_19.jpeg',
+		'img/gallery/img_20.jpeg',
+		'img/gallery/img_21.jpeg',
+		'img/gallery/img_22.jpeg',
+		'img/gallery/img_23.jpeg',
+		'img/gallery/img_24.jpeg',
+		'img/gallery/img_25.jpeg',
+		'img/gallery/img_26.jpeg',
+		'img/gallery/img_27.jpeg',
 	];
 
 	return (
 		<div className="section improve-skills">
-			<div className="col img">
+			{/* <div className="col img">
 				<img src="/img/gallery/img_10.jpeg" alt="sushi" />
+			</div> */}
+			<div className="col gallery">
+				{images.map((src, index) => (
+					<CustomImage key={index} imgSrc={src} pt={'90%'} />
+				))}
 			</div>
 			<div className="col typography">
 				<h1 className="title">Improve Your Culinary Skills</h1>
