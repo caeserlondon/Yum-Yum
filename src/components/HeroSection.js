5a0052fe79d01c582ca5a15a5c6085f0df5e265e
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-20 21:38:36 +0100

    Deleting the semicolon and double quotes and updating the images

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,4 +1,4 @@
-import CustomImage from './CustomImage';
+import CustomImage from './CustomImage'
 
 export default function HeroSection() {
 	const images = [
@@ -6,36 +6,36 @@ export default function HeroSection() {
 		'img/gallery/img_2.jpeg',
 		'img/gallery/img_3.jpeg',
 		'img/gallery/img_4.jpeg',
-		'img/gallery/img_5.jpeg',
+		'img/gallery/img_5.gif',
 		'img/gallery/img_6.jpeg',
 		'img/gallery/img_7.jpeg',
 		'img/gallery/img_8.jpeg',
 		'img/gallery/img_9.jpeg',
-	];
+	]
 	const images2 = [
 		'img/gallery/img_10.jpeg',
 		'img/gallery/img_11.jpeg',
 		'img/gallery/img_12.jpeg',
 		'img/gallery/img_13.jpeg',
-		'img/gallery/img_14.jpeg',
+		'img/gallery/img_14.gif',
 		'img/gallery/img_15.jpeg',
 		'img/gallery/img_16.jpeg',
 		'img/gallery/img_17.jpeg',
 		'img/gallery/img_18.jpeg',
-	];
+	]
 	return (
-		<div className="section hero">
-			<div className="col gallery">
+		<div className='section hero'>
+			<div className='col gallery'>
 				{images.map((src, index) => (
 					<CustomImage key={index} imgSrc={src} pt={'90%'} />
 				))}
 			</div>
 
-			<div className="col gallery second">
+			<div className='col gallery second'>
 				{images2.map((src, index) => (
 					<CustomImage key={index} imgSrc={src} pt={'90%'} />
 				))}
 			</div>
 		</div>
-	);
+	)
 }
