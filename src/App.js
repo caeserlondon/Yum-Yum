f04bf99f10c8bb58ca5830b952311de18b66607a
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-20 21:40:57 +0100

    Deleting the semicolon and double quotes & adding the error page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Home from "./pages/Home";
-import Recipes from "./pages/Recipes";
-import Settings from "./pages/Settings";
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import Recipes from './pages/Recipes'
+import Settings from './pages/Settings'
+import Error from './pages/Error'
 
 function App() {
 	return (
 		<Router>
 			<Navbar />
-			<div className="container main">
+			<div className='container main'>
 				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/recipes" element={<Recipes />} />
-					<Route path="/settings" element={<Settings />} />
+					<Route path='/' element={<Home />} />
+					<Route path='/recipes' element={<Recipes />} />
+					<Route path='/settings' element={<Settings />} />
+					<Route path='/error' element={<Error />} />
 				</Routes>
 			</div>
 			<Footer />
 		</Router>
-	);
+	)
 }
 
-export default App;
+export default App
