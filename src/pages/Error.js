fad9d2818b509429d5059b6ee559f279a34bc9b9
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-20 21:40:06 +0100

    Creating the error page

---
+++ src/pages/Error.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Error = () => {
+	return (
+		<>
+			<div className='errorPage'>
+				<h1>Data Limit Reached</h1>
+				<h1>
+					We are very sorry our API suplier (Spoonacular API ) give us only 150
+					points/day then no more calls.
+				</h1>
+				<h2>Thank you for understanding and Please come back tommorw...</h2>
+			</div>
+		</>
+	)
+}
+
+export default Error
