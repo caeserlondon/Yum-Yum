7a8586a26488c0f39ed5a5733966a2a29cac12ee
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-30 21:01:14 +0100

    Updating the component code

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,21 +1,23 @@
-import CustomImage from "./CustomImage";
+import CustomImage from './CustomImage'
+import { Link } from 'react-router-dom'
 
 export default function RecipeCard({ recipe }) {
 	return (
-		<div className="recipe-card">
-			<CustomImage imgSrc={recipe.image} pt="65%" />
-			<div className="recipe-card-info">
-				<img className="author-img" src={recipe.authorImg} alt="chief" />
-				<p className="recipe-title">{recipe.title}</p>
-				<p className="recipe-desc">
-					Spicy jalapeno tail drumstick sirloin short loin jerky steak doner
-					pastrami meatloaf swine, rump meatball pork belly spare ribs shoulder
-					pork chicken drumstick.
-				</p>
-				<a className="view-btn" href="#!">
-					VIEW RECIPE
-				</a>
+		<div className='recipe-card'>
+			<CustomImage imgSrc={recipe.image} pt='65%' />
+			<div className='recipe-card-info'>
+				<p className='recipe-title'>{recipe.title}</p>
+				<div className='recipe-desc'>
+					{/* {recipe.dishTypes.map((type) => {
+						return <p key={type}>{type}</p>
+					})} */}
+				</div>
+				<div>
+					<Link className='view-btn' to={'/recipe/' + recipe.id}>
+						VIEW RECIPE
+					</Link>
+				</div>
 			</div>
 		</div>
-	);
+	)
 }
