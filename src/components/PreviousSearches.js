36fa12180f1990216a53225fed8ae85dff017a72
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-30 21:03:24 +0100

    Updating the component

--- src/components/PreviousSearches.js
+++ src/components/PreviousSearches.js
@@ -1,43 +1,46 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import Cuisines from './Cuisines'
 
-const searches = [
-	"pizza",
-	"ramen",
-	"cookies",
-	"Taco",
-	"biriyani",
-	"salad",
-	"Burger",
-	"steak",
-	"pudding",
-	"soup",
-	"sushi",
-	"barbecue",
-	"pasta",
-];
+// const searches = [
+// 	'pizza',
+// 	'ramen',
+// 	'cookies',
+// 	'Taco',
+// 	'biriyani',
+// 	'salad',
+// 	'Burger',
+// 	'steak',
+// 	'pudding',
+// 	'soup',
+// 	'sushi',
+// 	'barbecue',
+// 	'pasta',
+// ]
 
 export default function PreviousSearches() {
 	return (
-		<div className="previous-searches section">
-			<h2>Previous Searches</h2>
-			<div className="previous-searches-container">
+		<>
+			<Cuisines />
+			<div className='previous-searches section'>
+				{/* <div className='previous-searches-container'>
 				{searches.map((search, index) => (
 					<div
-						className="search-item"
-						key={index}
-						style={{ animationDelay: index * 0.15 + "s" }}
+					className='search-item'
+					key={index}
+					style={{ animationDelay: index * 0.15 + 's' }}
 					>
-						{search}
+					{search}
 					</div>
-				))}
+					))}
+				</div> */}
+				<div className='search-box'>
+					<input type='text' placeholder='Search' />
+					<button className='btn'>
+						<FontAwesomeIcon icon={faSearch} />
+					</button>
+				</div>
 			</div>
-			<div className="search-box">
-				<input type="text" placeholder="Search" />
-				<button className="btn">
-					<FontAwesomeIcon icon={faSearch} />
-				</button>
-			</div>
-		</div>
-	);
+		</>
+	)
 }
