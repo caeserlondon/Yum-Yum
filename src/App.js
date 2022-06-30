aae102e1809edf99caad90785f1635a526276dc8
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-30 21:02:01 +0100

    Adding the cuisine page and Route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './pages/Home'
 import Recipes from './pages/Recipes'
+import CuisinesPage from './pages/CuisinesPage'
 import Settings from './pages/Settings'
 import Error from './pages/Error'
 import LimitReached from './pages/LimitReached'
@@ -16,6 +17,7 @@ function App() {
 				<Routes>
 					<Route path='/' element={<Home />} />
 					<Route path='/recipes' element={<Recipes />} />
+					<Route path='/cuisine/:type' element={<CuisinesPage />} />
 					<Route path='/settings' element={<Settings />} />
 					<Route path='/*' element={<Error />} />
 					<Route path='/limit-reached' element={<LimitReached />} />
