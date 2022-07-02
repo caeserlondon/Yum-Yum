f92677e12b32931baccc713d335292071d42865b
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-07-02 09:06:49 +0100

    Adding functunality to the search input

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,19 +1,36 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import Cuisines from './Cuisines'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function PreviousSearches() {
+	const [input, setInput] = useState('')
+
+	const navigate = useNavigate()
+
+	const submitHandler = (e) => {
+		e.preventDefault()
+
+		navigate('/searched/' + input)
+	}
+
 	return (
 		<>
 			<Cuisines />
-			<div className='previous-searches section'>
+			<form onSubmit={submitHandler} className='previous-searches section'>
 				<div className='search-box'>
-					<input type='text' placeholder='Search' />
+					<input
+						type='text'
+						placeholder='Search'
+						onChange={(e) => setInput(e.target.value)}
+						value={input}
+					/>
 					<button className='btn'>
 						<FontAwesomeIcon icon={faSearch} />
 					</button>
 				</div>
-			</div>
+			</form>
 		</>
 	)
 }
