ba7aaaa2d4a5ea5220be9dffa53fa7e367e0d839
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-07-04 19:05:07 +0100

    Updating the Error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,17 @@
-import IMG from '../assets/error.gif'
+import { FaGhost } from 'react-icons/fa'
 
 const Error = () => {
 	return (
-		<div className='errorPage'>
-			<img src={IMG} alt='error' />
+		<div className='error-Page'>
+			<h1>
+				4
+				<span>
+					<FaGhost />
+				</span>
+				4
+			</h1>
+			<h2>Error: 404 page not found</h2>
+			<p>We are sorry, But the page you are looking for does not exist.</p>
 		</div>
 	)
 }
