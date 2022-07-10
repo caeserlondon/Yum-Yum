638828be0971e3b7a14018ae0913d2bc1494d824
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-07-10 12:22:49 +0100

    Updating the Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,29 +1,30 @@
 export default function Footer() {
+	const footerYear = new Date().getFullYear()
 	return (
-		<div className="footer container">
-			<div className="footer-section">
-				<p className="title">YUM YUM</p>
+		<div className='footer container'>
+			<div className='footer-section'>
+				<p className='title'>YUM YUM</p>
 				<p>
 					YUM YUM is a place where you can please your soul and tummy with
 					delicious food recepies of all cuisine.
 				</p>
-				<p>&copy; 2022 | All Rights Reserved</p>
+				<p>Copyrights &copy; {footerYear} Yum-Yum | All Rights Reserved</p>
 			</div>
 
-			<div className="footer-section">
-				<p className="title">Contact Us</p>
+			<div className='footer-section'>
+				<p className='title'>Contact Us</p>
 				<p>yumyum@gmail.co.uk</p>
 				<p>+44-987-654-321</p>
 				<p>666 Hyde Park Corner</p>
 				<p>W2 2UH</p>
 			</div>
 
-			<div className="footer-section">
-				<p className="title">Socials</p>
+			<div className='footer-section'>
+				<p className='title'>Socials</p>
 				<p>FaceBook</p>
 				<p>Twitter</p>
 				<p>Instagram</p>
 			</div>
 		</div>
-	);
+	)
 }
