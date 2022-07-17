9e422604b31781154e1566176cabfdcf21c19cb7
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-07-17 20:42:59 +0100

    Deleting the photos

--- src/components/ImproveSkills.js
+++ src/components/ImproveSkills.js
@@ -1,5 +1,4 @@
 import { Link } from 'react-router-dom'
-import CustomImage from './CustomImage'
 
 export default function ImproveSkills() {
 	const list = [
@@ -10,25 +9,9 @@ export default function ImproveSkills() {
 		'Get cooking tips',
 		'Eat healthy',
 	]
-	const images = [
-		'img/gallery/img_19.gif',
-		'img/gallery/img_20.gif',
-		'img/gallery/img_21.gif',
-		'img/gallery/img_22.gif',
-		'img/gallery/img_23.gif',
-		'img/gallery/img_24.gif',
-		'img/gallery/img_25.gif',
-		'img/gallery/img_26.gif',
-		'img/gallery/img_27.gif',
-	]
 
 	return (
 		<div className='section improve-skills'>
-			<div className='col gallery'>
-				{images.map((src, index) => (
-					<CustomImage key={index} imgSrc={src} pt={'90%'} />
-				))}
-			</div>
 			<div className='col typography'>
 				<h1 className='title'>Improve Your Culinary Skills</h1>
 				{list.map((item, index) => (
